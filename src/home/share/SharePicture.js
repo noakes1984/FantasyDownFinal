153ccaa5f27969eec39d422b2363a82cf0a0d1ad
William Candillon <wcandillon@gmail.com>
2017-12-07 08:31:46 +0100

    1️⃣

--- src/home/share/SharePicture.js
+++ src/home/share/SharePicture.js
@@ -5,11 +5,15 @@ import * as React from "react";
 import {
     StyleSheet, TextInput, Image, Dimensions, KeyboardAvoidingView, ScrollView, ImageEditor, ImageStore, View
 } from "react-native";
+import {FileSystem} from "expo";
 
-import {Container, NavHeader, Button, Theme, APIStore, RefreshIndicator, NavigationHelpers} from "../../components";
+import {Container, NavHeader, Button, Theme, APIStore, RefreshIndicator, NavigationHelpers, Firebase} from "../../components";
+import SHA1 from "crypto-js/sha1";
 
 import type {Picture} from "./Picture";
 import type {ScreenParams} from "../../components/Types";
+
+import {atob, convertToByteArray, uploadAsByteArray} from "../../components/UploadHelper";
 
 const id = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 
@@ -71,7 +75,19 @@ export default class SharePicture extends React.Component<ScreenParams<Picture>,
                 preview
             }
         });
-        NavigationHelpers.reset(navigation, "Home");
+        try {
+            const task = Firebase.storage.ref("pictures").put(convertToByteArray(preview), { contentType: "image/jpeg"});
+            task.on("state_changed", snapshot => {
+
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+
+            }, error => console.error(error), () => console.log("DONE"));
+        } catch(e) {
+            console.log("ERROR");
+            console.log(e);
+        }
+        // NavigationHelpers.reset(navigation, "Home");
     }
 
     @autobind

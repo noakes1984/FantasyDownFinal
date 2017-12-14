a631caba37b8b08f2cb611f1350932040df3c06c
William Candillon <wcandillon@gmail.com>
2017-12-14 15:24:12 +0100

    Firebase review (#55)

--- src/sign-up/Email.js
+++ src/sign-up/Email.js
@@ -38,7 +38,7 @@ export default class Email extends React.Component<NavigationProps<*>, EmailStat
     render(): React.Node {
         const {navigation} = this.props;
         return (
-            <SignUpContainer title="Your Email" subtitle="We won't span" next={this.next} {...{ navigation }}>
+            <SignUpContainer title="Your Email" subtitle="We won't spam" next={this.next} {...{ navigation }}>
                 <TextField
                     placeholder="Email"
                     keyboardType="email-address"
